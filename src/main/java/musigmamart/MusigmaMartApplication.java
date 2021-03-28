e9d4762f9c5d7fe75fde2cfb3e813e7385a607f9
Abhishek Nag <abhishek123nag@gmail.com>
2021-03-28 14:08:29 +0530

    First Commit

---
+++ src/main/java/musigmamart/MusigmaMartApplication.java
@@ -0,0 +1,13 @@
+package musigmamart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusigmaMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusigmaMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/musigmamart/MusigmaMartApplicationTests.java
@@ -0,0 +1,13 @@
+package musigmamart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusigmaMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
